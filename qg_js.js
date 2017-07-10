350ad3af9bc02272b98cf676b962b88c8aa556a4
Ron Laniado <rlaniado24@gmail.com>
2017-07-09 20:36:48 -0400

    More fixes

--- qg_js.js
+++ qg_js.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   console.log("Hey fam");
 function getNewQuote() {
   $.ajax({
-    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+    url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
     jsonp: 'jsonp',
     success: function(data) {
       var post = data.shift();
