e929fb3cb6b4c820dd56a6e8ef39206e8b323e00
Ron Laniado <rlaniado24@gmail.com>
2017-07-16 23:52:56 -0400

    Took out console.log() tests

--- qg_js.js
+++ qg_js.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log("Hey fam");
 function getNewQuote() {
   $.ajax({
     url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
@@ -15,7 +14,6 @@ function getNewQuote() {
   });
 }
 function changeBackground() {
-  console.log("changeBackground() function is working");
   var backgrounds = ["navy", "blue", "aqua", "teal", "olive", "green", "lime", "yellow", "orange", "red", "maroon", "fuchsia", "purple"];
   var randomBG = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   $("html").css('background-color',randomBG);}
